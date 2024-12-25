caa673cd5ebc964b4520864ecd3323aa889d033f
MarijnSt <marijn.stammeleer@gmail.com>
2024-12-25 11:54:30 +0100

    feat: histogram of pickups

--- uber_pickups.py
+++ uber_pickups.py
@@ -8,9 +8,6 @@ st.title("Uber Pickups in NYC")
 DATE_COLUMN = "date/time"
 DATA_URL = "https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
 
-# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-#          'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
 @st.cache_data
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -21,6 +18,12 @@ def load_data(nrows):
 
 data_load_state = st.text('Loading data...')
 data = load_data(1000)
-data_load_state.text("Done! (using st.cache_data)")
+data_load_state.text("Loading data... Done! (using st.cache_data)")
 
-st.dataframe(data)
+st.subheader("Raw data")
+st.write(data)
+
+st.subheader("Number of pickups by hour")
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
